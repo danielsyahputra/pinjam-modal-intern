d484bb83502b0146b176309dc54d43becd898d9f
danielsyahputra13 <dnlshp@gmail.com>
2021-12-05 22:39:26 +0700

    add validator

--- ts-postgress/src/index.ts
+++ ts-postgress/src/index.ts
@@ -6,6 +6,7 @@ import {Request, Response} from "express";
 import { Routes } from "./routes";
 import { port } from "../src/config";
 import * as morgan from "morgan";
+import { validationResult } from "express-validator";
 
 function handleError(err, req, res, next) {
     res.status(err.statusCode || 500).send({ message: err.message });
@@ -19,8 +20,14 @@ createConnection().then(async connection => {
     app.use(bodyParser.json());
 
     Routes.forEach(route => {
-        (app as any)[route.method](route.route, async (req: Request, res: Response, next: Function) => {
+        (app as any)[route.method](route.route,
+            ...route.validation,
+            async (req: Request, res: Response, next: Function) => {
             try {
+                const errors = validationResult(req);
+                if (!errors.isEmpty()) {
+                    return res.status(400).json({errors: errors.array()})
+                }
                 const result = await (new (route.controller as any))[route.action](req, res, next);
                 res.json(result);
             } catch (error) {
