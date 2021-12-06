96dd85a0a78110b21b121947104f72cb36e5bacc
danielsyahputra13 <dnlshp@gmail.com>
2021-12-06 23:28:29 +0700

    connect to postgres

--- ts-postgres-sequelize/src/index.ts
+++ ts-postgres-sequelize/src/index.ts
@@ -1,2 +1,19 @@
-console.log("Hello world!");
-console.log("Hallo");
+import express, { NextFunction, Request, Response } from "express";
+import { port } from "./config";
+import db from "./config/database.config";
+
+db.authenticate();
+
+db.sync().then(async () => {
+    console.log("Connect to db");
+})
+
+const app = express();
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    return res.status(200).send("Hello world!");
+})
+
+app.listen(port, () => {
+    console.log(`Server is runing on port ${port}`)
+});
